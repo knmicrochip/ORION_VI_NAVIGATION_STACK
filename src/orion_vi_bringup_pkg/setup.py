05fe3d3cf0b84ccd88206463c7a9210e26be222b
ludrol <jot.s.gam@gmail.com>
2026-02-26 17:43:48 +0100

    fix: use python package instead of cmake package

---
+++ src/orion_vi_bringup_pkg/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'orion_vi_bringup_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ludrol',
+    maintainer_email='jot.s.gam@gmail.com',
+    description='TODO: Package description',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
