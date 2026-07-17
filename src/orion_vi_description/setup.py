8dec7f4c879be1b0dacbafb33cf90edba22ba40f
Bungok <ignacy@piekarczyk.pl>
2026-07-17 10:46:43 +0200

    feat: converted imu into a macro

--- src/orion_vi_description/setup.py
+++ src/orion_vi_description/setup.py
@@ -15,6 +15,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.sdf')),
+        (os.path.join('share', package_name, 'urdf', 'sensors'), glob('urdf/sensors/*.xacro')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
